83cbd56ef5adf8d640dd213d9a5a009aa5e69ba2
Duyile Tolulope Patrick <princericky2000@yahoo.com>
2016-11-19 19:24:53 +0100

    design with splash

---
+++ PlayDiary/app/src/main/java/com/example/playdiary/playdiary/ActivitySignIn.java
@@ -0,0 +1,16 @@
+package com.example.playdiary.playdiary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Engr. TeeBoy on 19/11/2016.
+ */
+public class ActivitySignIn  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+    }
+}
---
+++ PlayDiary/app/src/main/java/com/example/playdiary/playdiary/ActivitySplash.java
@@ -0,0 +1,78 @@
+package com.example.playdiary.playdiary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivitySplash extends AppCompatActivity {
+    Handler handler;
+    Button signInButton, signUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        signInButton = (Button)findViewById(R.id.signInButton);
+        signUpButton = (Button)findViewById(R.id.signUpButton);
+        signInButton.setOnTouchListener(new ButtonTouchListener());
+        signUpButton.setOnTouchListener(new ButtonTouchListener());
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ActivitySplash.this, ActivitySignIn.class);
+                startActivity(intent);
+            }
+        });
+        handler = new Handler(Looper.getMainLooper());
+
+    }
+
+    private class ButtonTouchListener implements View.OnTouchListener{
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
+                Button source = (Button)v;
+                source.setAlpha((float)0.6);
+            }
+            else{
+                if(event.getAction() == MotionEvent.ACTION_UP){
+                    Button source = (Button)v;
+                    source.setAlpha((float)1.0);
+                }
+            }
+            return false;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
